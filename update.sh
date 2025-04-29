45e36f2260dfbbd79282fb46d745378ad06ffb42
cont1nuity <continuity@gmx.net>
2025-04-29 16:32:37 +0200

    - but keep cleanup

--- update.sh
+++ update.sh
@@ -263,4 +263,4 @@ function update {
 while read -r file; do
 	update "$file"
 #done < <(find . -type f -iname '*.toc' | sed 's/^.\///')
-done < <(find -- *.toc)
+done < <(find -- *.toc | sed 's/^.\///')
